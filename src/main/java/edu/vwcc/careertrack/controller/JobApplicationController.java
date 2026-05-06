ea8c7958b9ff43b31d30392b59d49c8afe30f29b
ckincer93 <cnk230@email.vccs.edu>
2026-05-05 20:14:10 -0400

    Update JobApplicationController.java

--- src/main/java/edu/vwcc/careertrack/controller/JobApplicationController.java
+++ src/main/java/edu/vwcc/careertrack/controller/JobApplicationController.java
@@ -2,7 +2,10 @@ package edu.vwcc.careertrack.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import edu.vwcc.careertrack.model.JobApplication;
 import edu.vwcc.careertrack.service.JobApplicationService;
